d1c57af9892332df2ead8ea0d4465c621bd8ef4e
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-11-18 21:17:50 -0600

    (feat): Implementación de Posts and users

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -13,4 +13,92 @@ router.get('/categories/', async(req, res)=>{
   res.json(data)
 })
 
+// PRISMA MORE TOOLS QUERY BUILDER PRISMA
+router.get('/categories/getall', async(req, res)=>{
+  //UpdateMany 
+  // const dataUpdated = await prisma.category.updateMany({
+  //   where:{
+  //     name:'categories'
+  //   },
+  //   data:{
+  //     name:'todasss'
+  //   }
+  // })
+  // const dataDeleted = await prisma.category.deleteMany({
+  //   where:{
+  //     name:'categories'
+  //   },
+  //   data:{
+  //     name:'todasss'
+  //   }
+  // })
+  // // UPSERT 
+  // //Esto lo que hace es buscar por un dato y si esta ejecutar el update, si no existe ejecutar el create
+
+  // const upsert = await prisma.category.upsert({
+  //   where:{
+  //     name:'categories'
+  //   },
+  //   create:{
+  //     name:'categories',
+  //     id:5
+  //   },
+  //   update:{
+  //     name:'nuevocategories'
+  //   }
+  // })
+  // // JOIN IN CREATE WITH RELATIONS
+  // const user = await prisma.user.create({
+  //   data:{
+  //     name:'nuevo usuario',
+  //     email:"nuevo@usuario.com",
+  //     lastname:"lastname"
+  //   }
+  // })
+
+  // const post = await prisma.post.create({
+  //   data:{
+  //     title:'Nuevo usuario post',
+  //     content: "NUevo contenido del post",
+  //     // userId:user.id
+  //     user: {
+  //       connect:{
+  //         id:user.id
+  //       }
+  //     }
+  //     //CUALQUIERA DE ESTAS DOS OPCIONES FUNCIONA A LA PERFECCIÓN
+  //   }
+  // })
+
+  // TAMBIEN PODEMOS CREAR UNA RELACION DESDE OTRO POR EJEMPLO
+  //UN POST DESDE UNA CREACION DE UN USUARIO
+
+  await prisma.user.create({
+    data:{
+      name:'Luis',
+      email:"luis@gmail.com",
+      lastname:"Martinez",
+      posts:{
+        create:[
+          {
+            title:'Tutorial de prisma',
+            content:' Contenido desde le usuario'
+          },
+          {
+            title:'Tutorial de prisma',
+            content:' Contenido desde le usuario'
+          },
+          {
+            title:'Tutorial de prisma',
+            content:' Contenido desde le usuario'
+          }
+        ]
+      }
+    }
+  })
+
+  const posts = await prisma.post.findMany()
+
+  res.json(posts)
+})
 export default router;
